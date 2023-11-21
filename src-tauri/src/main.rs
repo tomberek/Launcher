dce10d4a835ea1e084dbfe1c6af13df5436438cb
Coffee-Nerd <rpgplayers.inc@gmail.com>
2023-11-21 20:34:54 +1100

    Set up dependencies

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,6 +2,13 @@
 mod desktop;
 
 fn main() {
-  #[cfg(desktop)]
-  desktop::main();
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}!", name)
 }
