c7caa724a108898e872dd9ee9111b3449c074b07
Eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2023-11-25 06:49:45 +0000

    Update main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,9 +1,23 @@
 #[cfg(desktop)]
+
+mod auth;
+
+use auth::auth as authRS;
+
+
+
+#[tauri::command]
+async fn auth() -> Result<(), String> {
+    authRS().await.map_err(|e| e.to_string())
+}
+
+
+
 mod desktop;
 
 fn main() {
     tauri::Builder::default()
-        .run(tauri::generate_context!())
+        .run(tauri::generate_context!(auth))
         .expect("error while running tauri application");
 }
 
