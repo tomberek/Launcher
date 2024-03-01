16735bacc41a316934a2aeaa558b2da6fe072172
eveeify <eveeg1971@gmail.com>
2024-03-02 01:02:12 +1100

    chore: removed sentry

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 import { defineConfig } from 'astro/config';
-import sentry from "@sentry/astro";
-import spotlightjs from "@spotlightjs/astro";
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sentry(), spotlightjs(), tailwind()],
+  integrations: [tailwind()],
   vite: {
     server: {
       port: 4321,
