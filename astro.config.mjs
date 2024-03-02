c4cc96a1a0de2d76c65c9013b69c14b54e827f9b
eveeify <eveeg1971@gmail.com>
2024-03-02 16:52:45 +1100

    chore: removed unessery components and fixed bugs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,22 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   vite: {
+    clearScreen: false,
     server: {
+      strictPort: true,
       port: 4321,
       watch: {
         ignored: ['**/target/**', '**/src-tauri/**']
       }
+    },
+    envPrefix: ['VITE_', 'TAURI_PLATFORM', 'TAURI_ARCH', 'TAURI_FAMILY', 'TAURI_PLATFORM_VERSION', 'TAURI_PLATFORM_TYPE', 'TAURI_DEBUG'],
+    build: {
+      // Tauri uses Chromium on Windows and WebKit on macOS and Linux
+      target: process.env.TAURI_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
+      // don't minify for debug builds
+      minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+      // produce sourcemaps for debug builds
+      sourcemap: !!process.env.TAURI_DEBUG
     }
-  }
+  },
 });
