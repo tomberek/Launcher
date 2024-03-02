9b61f29acb36e36f90413aa01233abf379d34283
eveeify <eveeg1971@gmail.com>
2024-03-03 01:13:34 +1100

    chore: removed unessserary deps and code

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,40 +1,11 @@
-use dotenv::dotenv;
-use std::env;
 use tauri::App;
-
-#[cfg(mobile)]
-mod mobile;
-#[cfg(mobile)]
-pub use mobile::*;
 
 #[tauri::command]
 async fn auth() {
-    dotenv().ok();
-
-    /*
-     * All Credits go to Minecraft-Essentails.
-     * Note: Minecraft-Essentails Is Licenced Under The Affero GPL 3.0
-     */
-
-    let client_id = "";
-    let client_secret = env::var("Client_Secret").expect("Client_Secret Is Expected");
-    let ouath = minecraft_essentials::Oauth::new(&client_id);
-    print!("URL: {}", ouath.url());
-
-    // let ouath_info = ouath.launch(false).await?;
 }
 
 #[tauri::command]
 fn launch() {
-
-    /*
-     * All Credits go to Minecraft-Essentails.
-     * Note: Minecraft-Essentails Is Licenced Under The Affero GPL 3.0
-     */
-
-    //TODO: Minecraft Launching Client.
-
-    // minecraft_essentials::minecraft::new()
 }
 
 pub type SetupHook = Box<dyn FnOnce(&mut App) -> Result<(), Box<dyn std::error::Error>> + Send>;
@@ -49,27 +20,18 @@ impl AppBuilder {
         Self::default()
     }
 
-    #[must_use]
     pub fn setup<F>(mut self, setup: F) -> Self
     where
         F: FnOnce(&mut App) -> Result<(), Box<dyn std::error::Error>> + Send + 'static,
     {
-        self.setup.replace(Box::new(setup));
+        self.setup = Some(Box::new(setup));
         self
     }
 
     pub fn run(self) {
         let setup = self.setup;
         tauri::Builder::default()
-            .plugin(tauri_plugin_notification::init())
-            .plugin(tauri_plugin_os::init())
-            .plugin(tauri_plugin_http::init())
-            .plugin(tauri_plugin_dialog::init())
-            .plugin(tauri_plugin_shell::init())
-            .plugin(tauri_plugin_fs::init())
             .plugin(tauri_plugin_clipboard_manager::init())
-            .plugin(tauri_plugin_process::init())
-            .plugin(tauri_plugin_global_shortcut::Builder::new().build())
             .invoke_handler(tauri::generate_handler![auth, launch])
             .setup(move |app| {
                 if let Some(setup) = setup {
